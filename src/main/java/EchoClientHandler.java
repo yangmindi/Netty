cc5b38082545ee6cc497c794b4edbf92b999431f
yangmindi <1946334058@qq.com>
2020-01-07 18:39:18 +0800

    客户端

---
+++ src/main/java/EchoClientHandler.java
@@ -0,0 +1,28 @@
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.CharsetUtil;
+
+import java.nio.Buffer;
+
+@ChannelHandler.Sharable
+public class EchoClientHandler extends SimpleChannelInboundHandler<Buffer> {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        ctx.writeAndFlush(Unpooled.copiedBuffer("Netty rocks", CharsetUtil.UTF_8));
+    }
+
+
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, Buffer buffer) throws Exception {
+        System.out.println(buffer.toString());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
