ac536aaebaa48d967d55f97a3b0c01b7206e538f
yangmindi <1946334058@qq.com>
2020-01-13 11:05:27 +0800

    服务器引导端

--- chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
+++ chapter2/Server/src/main/java/nia/chapter2/echoserver/EchoServer.java
@@ -1,5 +1,17 @@
 package nia.chapter2.echoserver;
 
+import com.sun.security.ntlm.Server;
+import com.sun.xml.internal.rngom.digested.DGroupPattern;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+import java.net.InetSocketAddress;
+
 public class EchoServer {
     private final int port;
 
@@ -7,12 +19,37 @@ public class EchoServer {
         this.port = port;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         if (args.length != 1) {
             System.err.println("Usage: " + EchoServer.class.getSimpleName() +
                     " <port>"
             );
             return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+        new EchoServer(port).start();
+    }
+
+    public void start() throws InterruptedException {
+        final EchoServerHandler serverHandler = new EchoServerHandler();
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(group).channel(NioServerSocketChannel.class).localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(serverHandler);
+                        }
+                    });
+            ChannelFuture f = b.bind().sync();
+            f.channel().closeFuture().sync();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            group.shutdownGracefully().sync();
         }
     }
 }
